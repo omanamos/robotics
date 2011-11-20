fb9a02368418f0b796e7c88bc1db3504234b33ff
Ryan Oman <omanamos@gmail.com>
2011-11-19 21:26:34 -0800

    added c++ thirft generated file, updated c# gen files

--- windows/RPC/rpc.thrift
+++ windows/RPC/rpc.thrift
@@ -1,7 +1,17 @@
 namespace csharp Communication
+namespace cpp communication
+
+struct Point {
+  1: i32 x,
+  2: i32 y,
+  3: i32 z,
+}
+
+struct PointCloud {
+  1: list<Point> points,
+}
 
 service Rpc {
-
    void ping(),
-   string test()
+   list<PointCloud> getObjects()   
 }
